abfbf8b8a82b055242f8cbfd8771cc541e88be49
mnmn32 <manisha.sharmaforbusiness@gmail.com>
2025-11-10 18:24:39 +0530

    Time: 3 ms (21.40%) | Memory: 45.6 MB (11.89%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,27 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates); // optional, helps avoid duplicates
-        List<List<Integer>>[] dp = new ArrayList[target + 1];
-        
-        for (int i = 0; i <= target; i++)
-            dp[i] = new ArrayList<>();
-        
-        dp[0].add(new ArrayList<>());
+         List<List<Integer>> res = new ArrayList<>();
 
-       
-        for (int num : candidates) {
-            for (int t = num; t <= target; t++) {
-                for (List<Integer> comb : dp[t - num]) {
-                   
-                    if (comb.isEmpty() || num >= comb.get(comb.size() - 1)) {
-                        List<Integer> newComb = new ArrayList<>(comb);
-                        newComb.add(num);
-                        dp[t].add(newComb);
-                    }
-                }
-            }
+        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
+        return res;        
+    }
+
+    private static void makeCombination(int[] candidates, int target, int idx, List<Integer> comb, int total, List<List<Integer>> res) {
+        if (total == target) {
+            res.add(new ArrayList<>(comb));
+            return;
         }
 
-        return dp[target];
+        if (total > target || idx >= candidates.length) {
+            return;
+        }
+
+        comb.add(candidates[idx]);
+        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
+        comb.remove(comb.size() - 1);
+        makeCombination(candidates, target, idx + 1, comb, total, res);
+    }    
     }
-}
